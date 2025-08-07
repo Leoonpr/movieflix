f12b337c598652aa9dacae6acb16c94cd215079d
Leo <rochaleo879@gmail.com>
2025-08-07 11:10:11 -0300

    fix: ajusta as pastas

--- src/main/java/br/com/movieflix/controller/MovieController.java
+++ src/main/java/br/com/movieflix/controller/MovieController.java
@@ -1,38 +1,13 @@
 package br.com.movieflix.controller;
 
-import java.util.List;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import br.com.movieflix.controller.request.MovieRequest;
-import br.com.movieflix.controller.response.MovieResponse;
-import br.com.movieflix.entity.Movie;
-import br.com.movieflix.mapper.MovieMapper;
 import br.com.movieflix.service.MovieService;
-
 
 @RestController
 @RequestMapping("/movieflix/movie")
 public class MovieController {
-
-    @Autowired
     private MovieService service;
 
-    @PostMapping
-    public ResponseEntity<MovieResponse> save(@RequestBody MovieRequest request) {
-        Movie savedMovie = service.saveMovie(MovieMapper.toMovie(request));
-        return ResponseEntity.ok(MovieMapper.toMovieResponse(savedMovie));
-    }
-
-    @GetMapping
-    public ResponseEntity<List<MovieResponse>> findAll() {
-       return ResponseEntity.ok(service.findAll().stream()
-       .map(movie -> MovieMapper.toMovieResponse(movie)).toList());
-    }
 }
