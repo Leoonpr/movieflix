5ab5e651ac97c2d94d808ca6fb583d61cc8ecca4
Leo <rochaleo879@gmail.com>
2025-08-08 14:19:28 -0300

    feat: adiciona o controlller inicial do Movie

--- src/main/java/br/com/movieflix/controller/MovieController.java
+++ src/main/java/br/com/movieflix/controller/MovieController.java
@@ -1,13 +1,45 @@
 package br.com.movieflix.controller;
 
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.movieflix.controller.request.MovieRequest;
+import br.com.movieflix.controller.response.MovieResponse;
+import br.com.movieflix.entity.Movie;
+import br.com.movieflix.mapper.MovieMapper;
 import br.com.movieflix.service.MovieService;
 
 @RestController
 @RequestMapping("/movieflix/movie")
 public class MovieController {
     private MovieService service;
+
+    @PostMapping
+    public ResponseEntity<MovieResponse> save(@RequestBody MovieRequest request) {
+        Movie savedMovie = service.saveMovie(MovieMapper.toMovie(request));
+        return ResponseEntity.ok(MovieMapper.toMovieResponse(savedMovie));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<MovieResponse>> findAll() {
+        return ResponseEntity.ok(service.findAll()
+        .stream()
+        .map(MovieMapper::toMovieResponse)
+        .toList());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<MovieResponse> findById(@PathVariable Long id) {
+        return service.findById(id)
+            .map(movie -> ResponseEntity.ok(MovieMapper.toMovieResponse(movie)))
+            .orElse(ResponseEntity.notFound().build());
+    }
 
 }
